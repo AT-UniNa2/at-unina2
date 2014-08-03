964f2b616070183914df269bf7022931750c8a4f
Pasquale Boemio <boemianrapsodi@gmail.com>
2014-08-03 09:22:57 +0200

    build structure

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    copy: {
+      dist: {
+        files: [
+          { 
+            expand: true,
+            flatten: false,
+            cwd: 'app/',
+            src: ['index.html', 'js/**', 'css/**', 'pic/**'],
+            dest: 'dist/'
+          },
+        ]
+      }
+    },
+
+    clean: {
+      dist: ['dist/', 'node_modules']
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('default', ['copy:dist']);
+}
