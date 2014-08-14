b02ab41dd4c17ceb1f652d84b79f58c6ecb2edd2
Pasquale Boemio <boemianrapsodi@gmail.com>
2014-08-14 11:56:21 +0200

    class template

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,30 @@ module.exports = function(grunt) {
       }
     },
 
+    htmlbuild: {
+      dist: {
+        src: 'app/lectures/*.html',
+        dest: 'dist/lectures/',
+        options: {
+          sections: {
+            menu: 'app/lectures/snippets/menu.snippet',
+            foot: 'app/lectures/snippets/foot.snippet',
+            head: 'app/lectures/snippets/head.snippet',
+            navbar: 'app/lectures/snippets/navbar.snippet'
+          }
+        }
+      }
+    },
+
     clean: {
-      dist: ['dist/', 'node_modules']
+      dist: ['dist/'],
+      all: ['dist/', 'node_modules']
     },
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-html-build');
 
-  grunt.registerTask('default', ['copy:dist']);
+  grunt.registerTask('default', ['copy:dist', 'htmlbuild:dist']);
 }
